99f038e1c8d0b578dddaed69a010645db04a5f9c
jeanPcr <jean.coding971@gmail.com>
2022-12-09 23:57:55 +0100

    pushing all the code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductViewComponent } from './product/product-view/product-view.component';
 import { ProfileComponent } from './profile/profile.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
   { path: 'product/:id', component: ProductViewComponent },
+  { path: 'all', component: ProductListComponent },
   { path: 'profile', component: ProfileComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
